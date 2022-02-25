e5e2666f69cb401dce795aa338fb4a8480600756
ThePromidius <thepromidiusyt@gmail.com>
2022-02-25 01:56:43 +0100

    Added kavita tracker

--- app/src/main/java/eu/kanade/tachiyomi/data/track/TrackManager.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/track/TrackManager.kt
@@ -7,6 +7,7 @@ import eu.kanade.tachiyomi.data.track.kitsu.Kitsu
 import eu.kanade.tachiyomi.data.track.komga.Komga
 import eu.kanade.tachiyomi.data.track.myanimelist.MyAnimeList
 import eu.kanade.tachiyomi.data.track.shikimori.Shikimori
+import eu.kanade.tachiyomi.data.track.kavita.Kavita
 
 class TrackManager(context: Context) {
 
@@ -17,6 +18,7 @@ class TrackManager(context: Context) {
         const val SHIKIMORI = 4
         const val BANGUMI = 5
         const val KOMGA = 6
+        const val KAVITA = 7
     }
 
     val myAnimeList = MyAnimeList(context, MYANIMELIST)
@@ -31,7 +33,9 @@ class TrackManager(context: Context) {
 
     val komga = Komga(context, KOMGA)
 
-    val services = listOf(myAnimeList, aniList, kitsu, shikimori, bangumi, komga)
+    val kavita = Kavita(context, KAVITA)
+
+    val services = listOf(myAnimeList, aniList, kitsu, shikimori, bangumi, komga, kavita)
 
     fun getService(id: Int) = services.find { it.id == id }
 
