7fedc11e9857a2d4aa96720e30445f20d63d5a2d
ThePromidius <thepromidiusyt@gmail.com>
2022-11-11 14:35:31 +0100

    Apply suggestions from code review - hardcoded strings to track name
    Co-authored-by: Andreas <andreas.everos@gmail.com>

--- app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsTrackingScreen.kt
+++ app/src/main/java/eu/kanade/presentation/more/settings/screen/SettingsTrackingScreen.kt
@@ -164,7 +164,7 @@ class SettingsTrackingScreen : SearchableSettings {
                             if (hasValidSourceInstalled) {
                                 trackManager.komga.loginNoop()
                             } else {
-                                context.toast(context.getString(R.string.enhanced_tracking_warning, "Komga"), Toast.LENGTH_LONG)
+                                context.toast(context.getString(R.string.enhanced_tracking_warning, context.getString(trackManager.komga.nameRes())), Toast.LENGTH_LONG)
                             }
                         },
                         logout = trackManager.komga::logout,
@@ -181,7 +181,7 @@ class SettingsTrackingScreen : SearchableSettings {
                             if (hasValidSourceInstalled) {
                                 trackManager.kavita.loginNoop()
                             } else {
-                                context.toast(context.getString(R.string.enhanced_tracking_warning, "Kavita"), Toast.LENGTH_LONG)
+                                context.toast(context.getString(R.string.enhanced_tracking_warning, context.getString(trackManager.kavita.nameRes())), Toast.LENGTH_LONG)
                             }
                         },
                         logout = trackManager.kavita::logout,
